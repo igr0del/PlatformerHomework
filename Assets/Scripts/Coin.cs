773c8d19507c956a16c6a9b11ce1d7507ec7c40c
IGRODELOFF <inshildr@gmail.com>
2023-09-08 13:47:59 +0300

    Replace2

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -2,17 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(SpriteRenderer))]
+[RequireComponent(typeof(Animator))]
 public class Coin : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
